e2ee7acf2176f5db6caf6982c139ba9ab1bbbdd1
Madina <madinabektayeva@gmail.com>
2018-05-07 00:55:42 +0900

    turns added

---
+++ README.md
@@ -0,0 +1,14 @@
+
+![ ](demo/treasureHunt2.gif)
+
+
+This is a simple android application to track user's path inside a building. It uses built-in android sensors to detect user's direction and count steps. 
+
+![ ](demo/th1.jpg)
+
+![ ](demo/th2.jpg)
+
+![ ](demo/th3.jpg)
+
+
+![ ](demo/treasureHunt.gif)
--- app/src/main/java/com/madinabektayeva/treasurehunt/MainActivity.java
+++ app/src/main/java/com/madinabektayeva/treasurehunt/MainActivity.java
@@ -14,7 +14,6 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -23,28 +22,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    ImageView compass;
-    TextView direction_field;
-    TextView steps_field;
-    CustomView customView;
+    private ImageView compass;
+    private TextView direction_field;
+    private TextView steps_field;
+    private CustomView customView;
 
-    Bitmap bitmap;
-    Bitmap pathMarkBitmap;
-    Canvas pathCanvas;
-    Resources res;
-    BitmapFactory.Options opt;
-    Paint paint;
+    private Bitmap bitmap;
+    private Bitmap pathMarkBitmap;
+    private Canvas pathCanvas;
+    private Resources res;
+    private BitmapFactory.Options opt;
+    private Paint paint;
 
 
-    boolean running = false;
+    private boolean running = false;
     private float startStepCount;
     private float prevStepCount;
     private float lastStepCount;
 
     private int x;
     private int y;
+    private int stepSize;
     private int stepDistance;
+    private String move;
 
+    private int degree;
     private float currentDegree;
 
     private static SensorManager sensorServiceCompass;
@@ -53,11 +55,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor stepCountSensor;
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -74,10 +74,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         startStepCount = -1;
         prevStepCount = 0;
         lastStepCount = 0;
-        stepDistance = 1; //TODO() stepDistance = 5
+        stepDistance = 1; //TODO() stepDistance
         currentDegree = 0f;
+        degree = 0;
+        move = "up";
+        stepSize = 35;
 
-        bitmap =  Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888);
+        bitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888);
         pathCanvas = new Canvas();
         res = getResources();
         opt = new BitmapFactory.Options();
@@ -85,19 +88,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         pathMarkBitmap = BitmapFactory.decodeResource(res, R.drawable.pathmark, opt);
         paint = new Paint();
 
-        x = (int) customView.getWidth()/2;
-        y = (int) customView.getHeight()/2;
-        Log.d("Log", "x: " + x);
-        Log.d("Log", "y: " + y);
+        x = (int) customView.getWidth() / 2;
+        y = (int) customView.getHeight() / 2;
 
         customView.post(new Runnable() {
             @Override
             public void run() {
-                x = (int) customView.getWidth()/2;
+                x = (int) customView.getWidth() / 2;
                 y = (int) customView.getHeight();
-                bitmap =  Bitmap.createBitmap(customView.getWidth(), customView.getHeight(), Bitmap.Config.ARGB_8888);
-                Log.d("Log", "x: " + x);
-                Log.d("Log", "y: " + y);
+                bitmap = Bitmap.createBitmap(customView.getWidth(), customView.getHeight(), Bitmap.Config.ARGB_8888);
+
             }
         });
 
@@ -131,7 +131,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
-            int degree = Math.round(sensorEvent.values[0]);
+            degree = Math.round(sensorEvent.values[0]);
             direction_field.setText(Integer.toString(degree) + (char) 0x00B0);
 
             RotateAnimation ra = new RotateAnimation(currentDegree, -degree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
@@ -143,33 +143,78 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             currentDegree = -degree;
         }
         if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-            //prevStepCount = finishStepCount;
             lastStepCount = sensorEvent.values[0];
-            if(startStepCount == -1){
+            if (startStepCount == -1) {
                 startStepCount = sensorEvent.values[0];
                 prevStepCount = startStepCount;
             }
             if (running) {
-                steps_field.setText("" + String.valueOf(sensorEvent.values[0]-startStepCount));
-            }
-            if(lastStepCount > prevStepCount+stepDistance){
-                prevStepCount = lastStepCount;
-                y = y-15;
-                updatePath();
+                steps_field.setText("" + String.valueOf(sensorEvent.values[0] - startStepCount));
             }
 
         }
+
+        if (lastStepCount > prevStepCount + stepDistance) {
+            prevStepCount = lastStepCount;
+            updateNextMove();
+            updatePath();
+        }
     }
 
-    public void updatePath(){
+
+    public void updateNextMove() {
+
+        if (0 < degree && degree <= 90) {
+            move = "left";
+        } else if (90 < degree && degree <= 180) {
+            move = "up";
+        } else if (180 < degree && degree <= 270) {
+            move = "right";
+        } else {
+            move = "down";
+        }
+
+        Log.d("Log", "Move: " + move);
+        Log.d("Log", "Degree: " + degree);
+
+        if (move.equals("up")) {
+            goUp();
+        } else if (move.equals("down")) {
+            goDown();
+        } else if (move.equals("left")) {
+            goLeft();
+        } else if (move.equals("right")) {
+            goRight();
+        }
+
+    }
+
+    public void goRight() {
+        x += stepSize;
+    }
+
+    public void goLeft() {
+        x -= stepSize;
+    }
+
+    public void goUp() {
+        y -= stepSize;
+    }
+
+    public void goDown() {
+        y += stepSize;
+    }
+
+    public void updatePath() {
+        Log.d("Log", "x: " + x);
+        Log.d("Log", "y: " + y);
         pathCanvas = new Canvas(bitmap);
         pathCanvas.drawBitmap(pathMarkBitmap, x, y, paint);
-
         customView.drawPathMark(x, y, bitmap);
     }
 
     public void onAccuracyChanged(Sensor sensor, int i) {
-        //
+
     }
 
 
